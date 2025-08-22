58cd4eda5e8a393d70a45eaa21a7750baaf9edd1
mjespelita <mjespelita.swashed@gmail.com>
2025-08-22 13:03:39 +0800

    updated

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
     <h1>Dashboard</h1>
-    <b>Hello, {{ Auth::user()->name }} ({{ Auth::user()->role }})</b>
+    <b class="top-b">Hello, {{ Auth::user()->name }} ({{ Auth::user()->role }})</b>
     <div class="row">
         <div class="col-md-4">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Total Sample Number</h5>
-                    <h1>
+                    <h1 class="dashboard-h1">
                         <i class="fas fa-smile"></i> 322
                     </h1>
                 </div>
