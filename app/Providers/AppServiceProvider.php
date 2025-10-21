c98a4fec2911862f28ebd7f837aa9cc9ca846d07
mjespelita <mjespelita.swashed@gmail.com>
2025-10-21 22:27:59 +0800

    added ngrok https force config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +25,7 @@ class AppServiceProvider extends ServiceProvider
         Gate::define('viewPulse', function (User $user) {
             return ($user->role === 'admin') ? true : false;
         });
+
+        // URL::forceScheme('https'); // uncomment this if ur using ngrok
     }
 }
